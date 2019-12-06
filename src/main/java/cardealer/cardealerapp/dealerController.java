b33a6f6f07166466166f0cdba7134c0ede488f5c
Patryk Nowicki <patryk.nowicki29@gmail.com>
2019-12-07 00:24:21 +0100

    dealerController

---
+++ src/main/java/cardealer/cardealerapp/dealerController.java
@@ -0,0 +1,33 @@
+package cardealer.cardealerapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class dealerController {
+    private carRepo carRepo;
+    @Autowired
+    public dealerController(carRepo carRepo){
+        this.carRepo = carRepo;
+    }
+    @RequestMapping("/add")
+    public String addCar(
+            @RequestParam("mark") String mark,
+            @RequestParam("carModel") String carModel,
+            @RequestParam("engineType") String engineType,
+            @RequestParam("capacity") String capacity,
+            @RequestParam("productionDate") String productionDate,
+            @RequestParam("keyNumber") String keyNumber,
+            @RequestParam("addictives") String addictives,
+            Model model)
+        throws Exception{
+        Car car = new Car(mark, carModel, engineType, capacity, productionDate, keyNumber, addictives, true);
+        System.out.println(car);
+        carRepo.save(car);
+        model.addAttribute("car", car);
+        return "View";
+    }
+}
