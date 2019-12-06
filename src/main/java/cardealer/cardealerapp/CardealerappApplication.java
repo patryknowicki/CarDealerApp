ec3649d4becefc81eaa56e309e2159e5ae607320
Patryk Nowicki <patryk.nowicki29@gmail.com>
2019-12-06 23:30:11 +0100

    Initial commit

---
+++ src/main/java/cardealer/cardealerapp/CardealerappApplication.java
@@ -0,0 +1,13 @@
+package cardealer.cardealerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardealerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardealerappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cardealer/cardealerapp/CardealerappApplicationTests.java
@@ -0,0 +1,13 @@
+package cardealer.cardealerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardealerappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
